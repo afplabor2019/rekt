2325d4249509ede5df28ced7cc365e8746807789
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2019-11-08 19:50:34 +0100

    Update home.php

--- NagyProjekt/TeamFinder/views/home.php
+++ NagyProjekt/TeamFinder/views/home.php
@@ -2,11 +2,11 @@
   exit;
 } ?>
 <div>
-  <table>
+  <table id="homeTable">
     <tr>
-      <td><a href="<?php echo route(['page' => 'csgo']); ?>">Counter Strike: Global Offensive</a></td>
-      <td><a href="<?php echo route(['page' => 'lol']); ?>">League of Legends</a></td>
-      <td><a href="<?php echo route(['page' => 'r6s']); ?>">Rainbow Six: Siege</a></td>
+      <td id="homeCSGO"><div id="homeCSGOA"><a id="homeCSGOA"  href="<?php echo route(['page' => 'csgo']); ?>">Counter Strike: Global Offensive</a></div></td>
+      <td id="homeLol"><div id="homeLolA"><a  href="<?php echo route(['page' => 'lol']); ?>">League of Legends</a></div></td>
+      <td id="homeR6s"><div id="homeR6sA"><a id="homeR6sA" href="<?php echo route(['page' => 'r6s']); ?>">Rainbow Six: Siege</a></div></td>
     </tr>
     <table>
 </div>

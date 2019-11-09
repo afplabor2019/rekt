18f7ad08608b2688459b71a009e08db091bc7e65
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2019-11-09 01:39:09 +0100

    Home
    created home css/scss

--- NagyProjekt/TeamFinder/views/home.php
+++ NagyProjekt/TeamFinder/views/home.php
@@ -3,10 +3,16 @@
 } ?>
 <div>
   <table id="homeTable">
-    <tr>
-      <td id="homeCSGO"><div id="homeCSGOA"><a id="homeCSGOA"  href="<?php echo route(['page' => 'csgo']); ?>">Counter Strike: Global Offensive</a></div></td>
-      <td id="homeLol"><div id="homeLolA"><a  href="<?php echo route(['page' => 'lol']); ?>">League of Legends</a></div></td>
-      <td id="homeR6s"><div id="homeR6sA"><a id="homeR6sA" href="<?php echo route(['page' => 'r6s']); ?>">Rainbow Six: Siege</a></div></td>
+    <tr id="homeTr">
+      <td>
+        <div id="homeCSGO" class="shadow-pop-csgo"><a id="homeCSGOA" href="<?php echo route(['page' => 'csgo']); ?>">Counter Strike: Global Offensive</a></div>
+      </td>
+      <td>
+        <div id="homeLol" class="shadow-pop-lol"><a id="homeLolA" href="<?php echo route(['page' => 'lol']); ?>">League Of Legends</a></div>
+      </td>
+      <td>
+        <div id="homeR6s" class="shadow-pop-r6s"><a id="homeR6sA" href="<?php echo route(['page' => 'r6s']); ?>">Rainbow Six Siege</a></div>
+      </td>
     </tr>
     <table>
 </div>

85a2cc5dc1469549bebb2a82bd82a9bc67f96d93
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2019-12-07 21:35:43 +0100

    8080/Teszt terv
    Updated tesztelési terv.docx
    Added file to :8080 port

---
+++ NagyProjekt/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DOMAIN', 'http://127.0.0.1:8080/TeamFinder/');
+define('BASE_PATH', __DIR__);
+
+define('DEBUG_MODE', true);
+
+
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'teamfinder');
